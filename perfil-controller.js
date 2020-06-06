683cd5878f0f4d2d262a1ef60b516e1f4588746a
Natalia <n.alvescabrera@gmail.com>
2020-06-05 23:28:17 -0300

    Criação de pagina mostrando dados

--- perfil-controller.js
+++
@@ -1,8 +0,0 @@
-function enviarForm(){
-    var nome = document.getElementById("nomeId");
-    var sobrenome = document.getElementById("sobrenomeId");
-
-    if(nome.value!= "" && sobrenome.value!=""){
-        alert('Ok!' + nome.value + 'os seus dados foram encaminhados com sucesso!');
-    }
-}
